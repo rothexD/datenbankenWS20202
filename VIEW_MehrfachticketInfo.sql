d3517faa401910f85714b2091a1073e83ff14271
expries <56935464+expries@users.noreply.github.com>
2020-11-29 18:45:47 +0000

    Update VIEW_MehrfachticketInfo.sql

--- VIEW_MehrfachticketInfo.sql
+++ VIEW_MehrfachticketInfo.sql
@@ -1,6 +1,6 @@
 --*********************************************************************
 --**
---** View: MehrfachticketInfo
+--** View: Mehrfach_Ticket_Info
 --** Developer: Samuel Fiedorowicz
 --** Description: Zeigt für Mehrfachtickets den Gültigkeitsbereich,
 --**              das Kaufdatum, den Preis und die Ticket ID an
@@ -8,8 +8,8 @@
 --*********************************************************************
 
 CREATE OR REPLACE
-VIEW MehrfachticketInfo AS
+VIEW mehrfach_ticket_info AS
 SELECT t.ticketID, t.preis, t.kaufdatum, mt.gueltig_ab, mt.gueltig_bis
-  FROM Mehrfachticket mt
-  JOIN Ticket t
+  FROM mehrfachticket mt
+  JOIN ticket t
     ON mt.fk_ticketID = t.ticketID;
