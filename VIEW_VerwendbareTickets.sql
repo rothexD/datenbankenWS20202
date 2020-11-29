c2ea251086657e21e1f532e94fa710ec3cd8dc21
Jakob List <if19b062@technikum-wien.at>
2020-11-29 15:58:44 +0100

    2 new Views: VerwendbareTickets and VerwendeteTickets

---
+++ VIEW_VerwendbareTickets.sql
@@ -0,0 +1,17 @@
+--*********************************************************************
+--**
+--** Table: verwendbare_tickets
+--** Developer: Jakob List
+--** Description: View that allows to look up all tickets which are still able to be used.
+--**
+--********************************************************************
+
+CREATE OR REPLACE 
+VIEW verwendbare_tickets AS
+SELECT t.ticketid, t.fk_ticket_artid AS ticket_artid, t.fk_personid AS personid, ot.fk_verbindungid AS Verbindung, mt.gueltig_ab, mt.gueltig_bis FROM ticket t
+LEFT JOIN one_time_ticket ot
+ON t.ticketid = ot.fk_ticketid
+LEFT JOIN mehrfachticket mt
+ON t.ticketid = mt.fk_ticketid
+WHERE kaufdatum >= CURRENT_TIMESTAMP OR (gueltig_bis > CURRENT_TIMESTAMP AND gueltig_ab < CURRENT_TIMESTAMP)
+ORDER BY t.ticketid asc;
