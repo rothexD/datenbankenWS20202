3b92f587380bd4839084d52a55906ab6b7b27d2f
rothexD <56934789+rothexD@users.noreply.github.com>
2020-12-02 08:48:39 +0100

    user verwaltung

---
+++ USERVERWALTUNG_scriptcreateandgrantrights.sql
@@ -0,0 +1,5 @@
+create user datenbankprojekt identified by password;
+grant connect to datenbankprojekt;
+
+----- now grant execute on all stored procedures here in form of  "GRANT EXECUTE ON b.procedure_name TO a"  while b is the user that has the procedure stored.
+ 
