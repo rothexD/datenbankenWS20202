335aa45697a37b4f2cb966d03a3ac0085501d24b
elisabeth <eglatz@gmx.at>
2020-12-02 16:22:50 +0100

    functions next arrival, next departure

---
+++ FUNCTION_get_next_arrival.sql
@@ -0,0 +1,41 @@
+--********************************************************************
+--**
+--** Function: f_get_next_arrival
+--** In: n_bahnhofID - ID from train station that
+--** Returns: verbindungsID (ID of next arrival)
+--** Developer: Elisabeth Glatz
+--** Description: selects next arrival from the given train station and returns ID of connection
+--**
+--********************************************************************
+
+CREATE OR REPLACE FUNCTION f_get_next_arrival(n_bahnhofID IN NUMBER)
+	RETURN NUMBER
+IS
+	n_next_ID NUMBER;
+BEGIN
+	SELECT verbindungID INTO n_next_ID FROM (
+		SELECT * FROM bahnhof 
+					JOIN bahnsteig ON bahnhof.bahnhofID = bahnsteig.fk_bahnhofID
+					JOIN verbindung ON bahnsteig.bahnsteigID = verbindung.fk_ankunft_bahnsteig
+					WHERE bahnhofID = n_bahnhofID
+						AND ankunft_uhrzeit >= CURRENT_TIMESTAMP
+					ORDER BY ankunft_uhrzeit ASC
+	) WHERE ROWNUM = 1;
+	
+	RETURN n_next_ID;
+	
+EXCEPTION
+	WHEN no_data_found THEN
+	dbms_output.put_line('Keine Verbindung mit dieser BahnhofID gefunden.');
+	RETURN -1;
+	
+	WHEN OTHERS THEN
+  	dbms_output.put_line('Fehler aufgetreten.');
+	RETURN -1;
+	
+END;
+/
+
+
+
+select f_get_next_arrival (2) from dual;
---
+++ FUNCTION_get_next_departure.sql
@@ -0,0 +1,41 @@
+--********************************************************************
+--**
+--** Function: f_get_next_departures
+--** In: n_bahnhofID - ID from train station that
+--** Returns: verbindungsID (ID of next departure)
+--** Developer: Elisabeth Glatz
+--** Description: selects next departure from the given train station and returns ID of connection
+--**
+--********************************************************************
+
+CREATE OR REPLACE FUNCTION f_get_next_departures(n_bahnhofID IN NUMBER)
+	RETURN NUMBER
+IS
+	n_next_ID NUMBER;
+BEGIN
+	SELECT verbindungID INTO n_next_ID FROM (
+		SELECT * FROM bahnhof 
+					JOIN bahnsteig ON bahnhof.bahnhofID = bahnsteig.fk_bahnhofID
+					JOIN verbindung ON bahnsteig.bahnsteigID = verbindung.fk_abfahrt_bahnsteig
+					WHERE bahnhofID = n_bahnhofID
+						AND abfahrt_uhrzeit >= CURRENT_TIMESTAMP
+					ORDER BY abfahrt_uhrzeit ASC
+	) WHERE ROWNUM = 1;
+	
+	RETURN n_next_ID;
+	
+EXCEPTION
+	WHEN no_data_found THEN
+	dbms_output.put_line('Keine Verbindung mit dieser BahnhofID gefunden.');
+	RETURN -1;
+	
+	WHEN OTHERS THEN
+  	dbms_output.put_line('Fehler aufgetreten.');
+	RETURN -1;
+	
+END;
+/
+
+
+
+select f_get_next_departures (2) from dual;
