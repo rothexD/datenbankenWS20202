ccb1b2fd26e0c78194886acd4bd102a82b5e448c
expries <56935464+expries@users.noreply.github.com>
2020-12-08 21:12:09 +0000

    Update VIEW_Verpflegung.sql

--- VIEW_Verpflegung.sql
+++ VIEW_Verpflegung.sql
@@ -4,17 +4,16 @@
 --** Developer: Samuel Fiedorowicz
 --** Description: View that allows to list every product that the train has.
 --**
---*********************************************************************
 
+--*********************************************************************
 CREATE OR REPLACE
 VIEW verpflegung AS
 SELECT z.*, p.name AS produkt
 FROM zug z
-  JOIN zug_hat_wagons zw
-    ON zw.fk_zugID = z.zugID
   JOIN wagon w
-    ON zw.fk_wagonID = w.wagonID
+    ON w.fk_zugID = z.zugID
   JOIN wagon_hat_produkt wv
     ON wv.fk_wagonID = w.wagonID
   JOIN produkt p
-    ON wv.fk_produktID = p.produktID;
+    ON wv.fk_produktID = p.produktID
+ORDER BY z.zugID;
