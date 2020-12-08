303821eecbceaafe1f4668245030ccf5802dfcdf
expries <56935464+expries@users.noreply.github.com>
2020-12-08 21:09:49 +0000

    Update VIEW_MehrfachticketInfo.sql

--- VIEW_MehrfachticketInfo.sql
+++ VIEW_MehrfachticketInfo.sql
@@ -9,7 +9,14 @@
 
 CREATE OR REPLACE
 VIEW mehrfach_ticket_info AS
-SELECT t.ticketID, t.preis, t.kaufdatum, mt.gueltig_ab, mt.gueltig_bis
+SELECT
+   p.personID, t.ticketID,
+   ta.bezeichnung AS ticket_typ,
+   t.preis, t.kaufdatum, mt.gueltig_ab, mt.gueltig_bis
 FROM mehrfachticket mt
   JOIN ticket t
-    ON mt.fk_ticketID = t.ticketID;
+    ON mt.fk_ticketID = t.ticketID
+  JOIN person p
+    ON p.personID = t.fk_personID
+  JOIN ticket_art ta
+    ON ta.ticket_artID = t.fk_ticket_artID;
