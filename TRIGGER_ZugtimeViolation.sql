ac8dcf4fdbe00e249c067e302230d86d22494231
Nicolas Klement <nicolas.klement@gmail.com>
2020-12-11 18:08:31 +0100

    Fix function and exception name

--- TRIGGER_ZugtimeViolation.sql
+++ TRIGGER_ZugtimeViolation.sql
@@ -12,15 +12,15 @@ CREATE OR REPLACE TRIGGER tr_br_i_single_con_at_time
 BEFORE INSERT ON verbindung
 FOR EACH ROW
 DECLARE
-	e_trainOnMultipleConnAtSameTime EXCEPTION;
+	e_trainOnMultipleConnSameTime EXCEPTION;
 	CURSOR trainHasConnections_cur IS SELECT * FROM verbindung WHERE fk_zugID = :new.fk_zugID ;
 	n_bool NUMBER;
 BEGIN
 	FOR v_result IN trainHasConnections_cur
 	LOOP
-		n_bool := validate_no_time_overlap(v_result.abfahrt_uhrzeit,v_result.ankunft_uhrzeit,:new.abfahrt_uhrzeit,:new.ankunft_uhrzeit);
+		n_bool := f_validate_no_time_overlap(v_result.abfahrt_uhrzeit,v_result.ankunft_uhrzeit,:new.abfahrt_uhrzeit,:new.ankunft_uhrzeit);
 		IF n_bool =0 THEN
-		  RAISE e_trainOnMultipleConnAtSameTime;
+		  RAISE e_trainOnMultipleConnSameTime;
 		END IF;
 	END LOOP;
 END;
